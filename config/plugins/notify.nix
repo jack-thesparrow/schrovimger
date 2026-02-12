57470eb7c73cf56428197884de9fceeb6d8edd30
Rahul Tudu <rahultudu4705@gmail.com>
2026-02-13 01:20:16 +0530

    suppressed the lsp initialization messages

--- config/plugins/notify.nix
+++ config/plugins/notify.nix
@@ -2,6 +2,19 @@
   vim.notify = {
     nvim-notify = {
       enable = true;
+
+      setupOpts = {
+        render = "wrapped-compact";
+        stages = "static";
+
+        open_raw = {
+          __raw = ''
+            function(win)
+              vim.api.nvim_win_set_config(win, {focusable = false})
+            end
+          '';
+        };
+      };
     };
   };
 }
