227470be4ac14a6829044cbf6cf932422092903f
Rahul Tudu <rahultudu4705@gmail.com>
2026-02-13 10:29:04 +0530

    added diagnostics

---
+++ config/utility/diagnostics.nix
@@ -0,0 +1,15 @@
+{
+  vim.diagnostics = {
+
+    nvim-lint = {
+      enable = true;
+      lint_after_save = true;
+    };
+
+    enable = true;
+
+    config = {
+      underline = true;
+    };
+  };
+}
